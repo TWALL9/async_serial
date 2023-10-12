538dd05fb4d12913b5eff89ef7a1e007865b02e9
Tom Wallis <thomaswallis0@gmail.com>
2023-10-11 21:43:30 -0400

    Add old KISS library

---
+++ src/kiss_serial/kiss_encode.cpp
@@ -0,0 +1,43 @@
+#include "async_serial/kiss_tnc.hpp"
+
+namespace async_serial
+{
+
+KissOutputStream::KissOutputStream()
+{
+  init();
+}
+
+void KissOutputStream::init()
+{
+  buf_.clear();
+  buf_.push_back(FEND);
+  buf_.push_back(0);
+}
+
+void KissOutputStream::addByte(const uint8_t b)
+{
+  switch (b)
+  {
+    case (FEND):
+      buf_.push_back(FESC);
+      buf_.push_back(TFEND);
+      break;
+    case (FESC):
+      buf_.push_back(FESC);
+      buf_.push_back(TFESC);
+      break;
+    default:
+      buf_.push_back(b);
+      break;
+  }
+}
+
+std::vector<uint8_t> KissOutputStream::getBuffer()
+{
+  std::vector<uint8_t> out(buf_);
+  out.push_back(FEND);
+  return out;
+}
+
+}  // namespace async_serial
